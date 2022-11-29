2a99917c824ac9fb14e941427b15d8bf7c0078d6
valerio <vcrini@gmail.com>
2022-11-29 16:59:51 +0100

    adding probe and refactoring

--- deploy.tf
+++ deploy.tf
@@ -2,19 +2,33 @@ data "aws_api_gateway_rest_api" "primary" {
   name = var.api_id
 }
 
-resource "aws_api_gateway_resource" "commesse_list2" {
+resource "aws_api_gateway_resource" "commesse" {
   rest_api_id = data.aws_api_gateway_rest_api.primary.id
   parent_id   = data.aws_api_gateway_rest_api.primary.root_resource_id
   path_part   = "commesse"
 }
+resource "aws_api_gateway_resource" "probe" {
+  rest_api_id = data.aws_api_gateway_rest_api.primary.id
+  parent_id   = data.aws_api_gateway_rest_api.primary.root_resource_id
+  path_part   = "probe"
+}
 #GET
-resource "aws_api_gateway_method" "commesse_list2" {
+resource "aws_api_gateway_method" "commesse" {
   rest_api_id          = data.aws_api_gateway_rest_api.primary.id
-  resource_id          = aws_api_gateway_resource.commesse_list2.id
+  resource_id          = aws_api_gateway_resource.commesse.id
   http_method          = "GET"
   authorization        = "COGNITO_USER_POOLS"
   authorizer_id        = aws_api_gateway_authorizer.standard.id
   authorization_scopes = ["openid"]
+  #request_parameters = {
+  #  "method.request.path.commesse-list-terraform" = true
+  #}
+}
+resource "aws_api_gateway_method" "probe" {
+  rest_api_id   = data.aws_api_gateway_rest_api.primary.id
+  resource_id   = aws_api_gateway_resource.probe.id
+  http_method   = "GET"
+  authorization = "NONE"
   #request_parameters = {
   #  "method.request.path.commesse-list-terraform" = true
   #}
@@ -30,8 +44,8 @@ resource "aws_api_gateway_authorizer" "standard" {
   provider_arns = data.aws_cognito_user_pools.standard.arns
 }
 resource "aws_api_gateway_usage_plan" "default" {
-  name         = "my-usage-plan"
-  description  = "my description"
+  name         = "default"
+  description  = "used for commesse and probe"
   product_code = "MYCODE"
 
   #quota_settings {
@@ -48,36 +62,62 @@ resource "aws_api_gateway_usage_plan" "default" {
     api_id = data.aws_api_gateway_rest_api.primary.id
     stage  = var.stage
     throttle {
-      # fix down with dynamic string
-      path        = "/${aws_api_gateway_resource.commesse_list2.path_part}/${aws_api_gateway_method.commesse_list2.http_method}"
+      # commesse
+      path        = "/${aws_api_gateway_resource.commesse.path_part}/${aws_api_gateway_method.commesse.http_method}"
       burst_limit = "3"
       rate_limit  = "2"
     }
+    throttle {
+      # probe
+      path        = "/${aws_api_gateway_resource.probe.path_part}/${aws_api_gateway_method.probe.http_method}"
+      burst_limit = "1"
+      rate_limit  = "1"
+    }
   }
+  # api_stages {
+  #   api_id = data.aws_api_gateway_rest_api.primary.id
+  #   stage  = var.stage
+  #   throttle {
+  #     # probe
+  #     path        = "/${aws_api_gateway_resource.probe.path_part}/${aws_api_gateway_method.probe.http_method}"
+  #     burst_limit = "1"
+  #     rate_limit  = "1"
+  #   }
+  # }
 
 }
 
 resource "aws_api_gateway_integration" "default" {
-  http_method             = aws_api_gateway_method.commesse_list2.http_method
-  resource_id             = aws_api_gateway_resource.commesse_list2.id
+  http_method             = aws_api_gateway_method.commesse.http_method
+  resource_id             = aws_api_gateway_resource.commesse.id
   rest_api_id             = data.aws_api_gateway_rest_api.primary.id
   type                    = "HTTP_PROXY"
   connection_type         = "VPC_LINK"
   connection_id           = var.vpc_link_id
   integration_http_method = "GET"
-  uri                     = "http://bitgdi-test-sandboxecs-inlb-c2ede020b1256ea8.elb.eu-west-1.amazonaws.com/${var.stage}/${aws_api_gateway_resource.commesse_list2.path_part}"
+  uri                     = "http://bitgdi-test-sandboxecs-inlb-c2ede020b1256ea8.elb.eu-west-1.amazonaws.com/${var.stage}/${aws_api_gateway_resource.commesse.path_part}"
+}
+resource "aws_api_gateway_integration" "probe" {
+  http_method             = aws_api_gateway_method.probe.http_method
+  resource_id             = aws_api_gateway_resource.probe.id
+  rest_api_id             = data.aws_api_gateway_rest_api.primary.id
+  type                    = "HTTP_PROXY"
+  connection_type         = "VPC_LINK"
+  connection_id           = var.vpc_link_id
+  integration_http_method = "GET"
+  uri                     = "http://bitgdi-test-sandboxecs-inlb-c2ede020b1256ea8.elb.eu-west-1.amazonaws.com/${var.stage}/${aws_api_gateway_resource.probe.path_part}"
 }
 resource "aws_api_gateway_method_response" "response_200" {
   rest_api_id     = data.aws_api_gateway_rest_api.primary.id
-  resource_id     = aws_api_gateway_resource.commesse_list2.id
-  http_method     = aws_api_gateway_method.commesse_list2.http_method
+  resource_id     = aws_api_gateway_resource.commesse.id
+  http_method     = aws_api_gateway_method.commesse.http_method
   status_code     = "200"
   response_models = { "application/json" : "Empty" }
 }
 resource "aws_api_gateway_integration_response" "default" {
   rest_api_id = data.aws_api_gateway_rest_api.primary.id
-  resource_id = aws_api_gateway_resource.commesse_list2.id
-  http_method = aws_api_gateway_method.commesse_list2.http_method
+  resource_id = aws_api_gateway_resource.commesse.id
+  http_method = aws_api_gateway_method.commesse.http_method
   status_code = aws_api_gateway_method_response.response_200.status_code
 }
 resource "aws_api_gateway_deployment" "default" {
@@ -93,9 +133,11 @@ resource "aws_api_gateway_deployment" "default" {
     #       It will stabilize to only change when resources change afterwards.
     redeployment = sha1(jsonencode([
       aws_api_gateway_integration.default.id,
-      aws_api_gateway_resource.commesse_list2.id,
-      aws_api_gateway_method.commesse_list2.id,
-      aws_api_gateway_usage_plan.default.id
+      aws_api_gateway_integration.probe.id,
+      aws_api_gateway_resource.commesse.id,
+      aws_api_gateway_method.commesse.id,
+      aws_api_gateway_resource.probe.id,
+      aws_api_gateway_method.probe.id
     ]))
   }
 
